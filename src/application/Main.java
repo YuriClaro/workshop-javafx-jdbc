061fc175edd0448da25e59ada668061edfbac76e
Yuri Claro <yuri.claro@outlook.com>
2023-03-02 11:29:40 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,20 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			
+			mainScene = new Scene(parent);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
