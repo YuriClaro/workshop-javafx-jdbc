6a5778c6aeafcf630ba512c7a9c13d8c8771e118
Yuri Claro <yuri.claro@outlook.com>
2023-03-08 14:45:19 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -9,4 +9,15 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String text) {
+		try {
+		return Integer.parseInt(text);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+
 }
